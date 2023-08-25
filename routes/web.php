ce7184ca55977c39389b8daf500143d812e394c2
l3on <l3onwebdev@gmail.com>
2023-08-25 17:34:44 +0200

    added home controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComicController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 * HOME
 -------------------------------------------*/
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 /* -----------------------------------------
