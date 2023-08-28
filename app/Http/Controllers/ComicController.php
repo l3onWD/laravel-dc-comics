f6627917d22e80c5c3a79e32a0626956ef69cb79
l3on <l3onwebdev@gmail.com>
2023-08-28 16:51:30 +0200

    added comic update logic

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -69,7 +69,11 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->all();
+
+        $comic->update($data);
+
+        return to_route('comics.show', $comic);
     }
 
     /**
