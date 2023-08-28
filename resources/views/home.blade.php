01534ece62bb13000aa38047033647393433147f
l3on <l3onwebdev@gmail.com>
2023-08-28 15:44:43 +0200

    fixed home style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,15 @@
 
 @section('main-content')
     {{-- Home Section --}}
-    <section id="comic" class="section pb-5">
+    <section id="comic" class="section text-center pb-5">
 
         <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="DC Heroes" class="section-img mb-2">
 
         <div class="container py-5">
 
-            <h2 class="mb-3">Welcome to DC Comics</h2>
+            <h2 class="mb-5">Welcome to DC Comics</h2>
 
-            <div class="text-center mt-2">
-                <a href="{{ route('comics.index') }}" class="btn btn-primary">VIEW CURRENT SERIES</a>
-            </div>
+            <a href="{{ route('comics.index') }}" class="btn btn-primary">VIEW CURRENT SERIES</a>
 
         </div>
 
