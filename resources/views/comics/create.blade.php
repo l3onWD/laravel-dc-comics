85b0edb25ac317ae5fec1b388a29701b9e7feed8
l3on <l3onwebdev@gmail.com>
2023-08-28 17:29:42 +0200

    included validation message on create form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -9,6 +9,9 @@
 
         <div class="container py-5">
 
+            {{-- Validation Messages --}}
+            @include('includes.components.validation')
+
             <h2 class="mb-4">Add Comic</h2>
 
             <form action="{{ route('comics.store') }}" method="POST">
@@ -17,70 +20,117 @@
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="title" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="title" name="title"
-                                placeholder="Comic Title">
+                            <input type="text" class="form-control  @error('title') is-invalid @enderror" id="title"
+                                name="title" placeholder="Comic Title">
+                            @error('title')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
-                            <textarea class="form-control" name="description" id="description" rows="5"></textarea>
+                            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
+                                rows="5"></textarea>
+                            @error('description')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="thumb" class="form-label">Image</label>
-                            <input type="url" class="form-control" id="thumb" name="thumb"
-                                placeholder="Comic Image Url">
+                            <input type="url" class="form-control @error('thumb') is-invalid @enderror" id="thumb"
+                                name="thumb" placeholder="Comic Image Url">
+                            @error('thumb')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price" min="0"
-                                value="0" step="0.01">
+                            <input type="number" class="form-control @error('price') is-invalid @enderror" id="price"
+                                name="price" min="0" value="0" step="0.01">
+                            @error('price')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="series" class="form-label">Series</label>
-                            <input type="text" class="form-control" id="series" name="series"
-                                placeholder="Comic Series">
+                            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"
+                                name="series" placeholder="Comic Series">
+                            @error('series')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="sale-date" class="form-label">Sale Date</label>
-                            <input type="date" class="form-control" id="sale-date" name="sale_date">
+                            <input type="date" class="form-control @error('sale_date') is-invalid @enderror"
+                                id="sale-date" name="sale_date">
+                            @error('sale_date')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="type" class="form-label">Type</label>
-                            <input type="text" class="form-control" id="type" name="type"
-                                placeholder="Comic Type">
+                            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"
+                                name="type" placeholder="Comic Type">
+                            @error('type')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="artists" class="form-label">Artists</label>
-                            <input type="text" class="form-control" id="artists" name="artists"
-                                placeholder="Comic Artists">
+                            <input type="text" class="form-control @error('artists') is-invalid @enderror" id="artists"
+                                name="artists" placeholder="Comic Artists">
+                            @error('artists')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-5">
                             <label for="writers" class="form-label">Writers</label>
-                            <input type="text" class="form-control" id="writers" name="writers"
-                                placeholder="Comic Writers">
+                            <input type="text" class="form-control @error('writers') is-invalid @enderror" id="writers"
+                                name="writers" placeholder="Comic Writers">
+                            @error('writers')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
