8f10529bf21f9bc6b36398bac9bf4e29712d4416
l3on <l3onwebdev@gmail.com>
2023-08-28 17:43:58 +0200

    added style validation classes

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -9,6 +9,9 @@
 
         <div class="container py-5">
 
+            {{-- Validation Messages --}}
+            @include('includes.components.validation')
+
             <h2 class="mb-4">Add Comic</h2>
 
             <form action="{{ route('comics.update', $comic) }}" method="POST">
@@ -18,71 +21,118 @@
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="title" class="form-label">Title</label>
-                            <input type="text" class="form-control" id="title" name="title"
-                                placeholder="Comic Title" value="{{ $comic->title }}">
+                            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                                name="title" placeholder="Comic Title" value="{{ $comic->title }}">
+                            @error('title')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
-                            <textarea class="form-control" name="description" id="description" rows="5">{{ $comic->description }}</textarea>
+                            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
+                                rows="5">{{ $comic->description }}</textarea>
+                            @error('description')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="thumb" class="form-label">Image</label>
-                            <input type="url" class="form-control" id="thumb" name="thumb"
-                                placeholder="Comic Image Url" value="{{ $comic->thumb }}">
+                            <input type="url" class="form-control @error('thumb') is-invalid @enderror" id="thumb"
+                                name="thumb" placeholder="Comic Image Url" value="{{ $comic->thumb }}">
+                            @error('thumb')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price" min="0"
-                                value="{{ $comic->price ?? '0' }}" step="0.01">
+                            <input type="number" class="form-control @error('price') is-invalid @enderror" id="price"
+                                name="price" min="0" value="{{ $comic->price ?? '0' }}" step="0.01">
+                            @error('price')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="series" class="form-label">Series</label>
-                            <input type="text" class="form-control" id="series" name="series"
-                                placeholder="Comic Series" value="{{ $comic->series }}">
+                            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"
+                                name="series" placeholder="Comic Series" value="{{ $comic->series }}">
+                            @error('series')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="sale-date" class="form-label">Sale Date</label>
-                            <input type="date" class="form-control" id="sale-date" name="sale_date"
-                                value="{{ $comic->sale_date }}">
+                            <input type="date" class="form-control @error('sale_date') is-invalid @enderror"
+                                id="sale-date" name="sale_date" value="{{ $comic->sale_date }}">
+                            @error('sale_date')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="mb-3">
                             <label for="type" class="form-label">Type</label>
-                            <input type="text" class="form-control" id="type" name="type"
-                                placeholder="Comic Type" value="{{ $comic->type }}">
+                            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"
+                                name="type" placeholder="Comic Type" value="{{ $comic->type }}">
+                            @error('type')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-3">
                             <label for="artists" class="form-label">Artists</label>
-                            <input type="text" class="form-control" id="artists" name="artists"
-                                placeholder="Comic Artists" value="{{ $comic->artists }}">
+                            <input type="text" class="form-control @error('artists') is-invalid @enderror" id="artists"
+                                name="artists" placeholder="Comic Artists" value="{{ $comic->artists }}">
+                            @error('artists')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="col-12">
                         <div class="mb-5">
                             <label for="writers" class="form-label">Writers</label>
-                            <input type="text" class="form-control" id="writers" name="writers"
-                                placeholder="Comic Writers" value="{{ $comic->writers }}">
+                            <input type="text" class="form-control @error('writers') is-invalid @enderror"
+                                id="writers" name="writers" placeholder="Comic Writers"
+                                value="{{ $comic->writers }}">
+                            @error('writers')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
 
