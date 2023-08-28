4613b20e38fd72695fa3215f575a12488bd282a1
l3on <l3onwebdev@gmail.com>
2023-08-28 17:46:35 +0200

    added old values inside form

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -22,7 +22,7 @@
                         <div class="mb-3">
                             <label for="title" class="form-label">Title</label>
                             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
-                                name="title" placeholder="Comic Title" value="{{ $comic->title }}">
+                                name="title" placeholder="Comic Title" value="{{ old('title', $comic->title) }}">
                             @error('title')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -35,7 +35,7 @@
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
                             <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
-                                rows="5">{{ $comic->description }}</textarea>
+                                rows="5">{{ old('description', $comic->description) }}</textarea>
                             @error('description')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -48,7 +48,7 @@
                         <div class="mb-3">
                             <label for="thumb" class="form-label">Image</label>
                             <input type="url" class="form-control @error('thumb') is-invalid @enderror" id="thumb"
-                                name="thumb" placeholder="Comic Image Url" value="{{ $comic->thumb }}">
+                                name="thumb" placeholder="Comic Image Url" value="{{ old('thumb', $comic->thumb) }}">
                             @error('thumb')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -61,7 +61,8 @@
                         <div class="mb-3">
                             <label for="price" class="form-label">Price</label>
                             <input type="number" class="form-control @error('price') is-invalid @enderror" id="price"
-                                name="price" min="0" value="{{ $comic->price ?? '0' }}" step="0.01">
+                                name="price" min="0" value="{{ old('price', $comic->price) ?? '0' }}"
+                                step="0.01">
                             @error('price')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -74,7 +75,7 @@
                         <div class="mb-3">
                             <label for="series" class="form-label">Series</label>
                             <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"
-                                name="series" placeholder="Comic Series" value="{{ $comic->series }}">
+                                name="series" placeholder="Comic Series" value="{{ old('series', $comic->series) }}">
                             @error('series')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -87,7 +88,7 @@
                         <div class="mb-3">
                             <label for="sale-date" class="form-label">Sale Date</label>
                             <input type="date" class="form-control @error('sale_date') is-invalid @enderror"
-                                id="sale-date" name="sale_date" value="{{ $comic->sale_date }}">
+                                id="sale-date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
                             @error('sale_date')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -100,7 +101,7 @@
                         <div class="mb-3">
                             <label for="type" class="form-label">Type</label>
                             <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"
-                                name="type" placeholder="Comic Type" value="{{ $comic->type }}">
+                                name="type" placeholder="Comic Type" value="{{ old('type', $comic->type) }}">
                             @error('type')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -113,7 +114,7 @@
                         <div class="mb-3">
                             <label for="artists" class="form-label">Artists</label>
                             <input type="text" class="form-control @error('artists') is-invalid @enderror" id="artists"
-                                name="artists" placeholder="Comic Artists" value="{{ $comic->artists }}">
+                                name="artists" placeholder="Comic Artists" value="{{ old('artists', $comic->artists) }}">
                             @error('artists')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -127,7 +128,7 @@
                             <label for="writers" class="form-label">Writers</label>
                             <input type="text" class="form-control @error('writers') is-invalid @enderror"
                                 id="writers" name="writers" placeholder="Comic Writers"
-                                value="{{ $comic->writers }}">
+                                value="{{ old('writers', $comic->writers) }}">
                             @error('writers')
                                 <div class="invalid-feedback">
                                     {{ $message }}
